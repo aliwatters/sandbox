608639b3d43174e219e949990a31b9c04ce3fcc3
aliwatters <ali.watters@gmail.com>
2014-06-20 00:44:41 +0000

    wip experiments

--- 2014/20140619-clientside-passhash/index.js
+++ 2014/20140619-clientside-passhash/index.js
@@ -1,23 +1,30 @@
 var Hashes = require('jshashes');
 
+// custom alphabet defined.
 window._app = {
-  Hashes : Hashes
+  Hashes : Hashes,
+  alphabet : 'abcdefghijklmnopqrstuvwzxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!#$%^&*()_+-={};":<>?,./'
 };
 console.log('jsHashes loaded');
 
 var str = 'This is a sample text!';
 // new SHA1 instance and base64 string encoding
-var SHA512 = new Hashes.SHA512().hex(str);
 
-// some stretching - 1024 to test
+var SHA512 = new Hashes.SHA512();
 
-var max = 1024;
+// some stretching - 10 to test
+var max = 10;
 var i;
+var code = str;
 
-console.time('stretching');
+console.time('testing');
 for (i = 0; i < max; i++) {
-  SHA512 = new Hashes.SHA512().hex(str);
+  code = SHA512.hex(code);
 }
-console.timeEnd('stretching');
+console.timeEnd('testing');
+if (code === '86d2244eab6d733d690a7cf746cf450a23acd002e650667d0fb04055952ef30d384a075f595818410911ec5cdd8733d9e9e4b3ffa3b9e3ca61d05935c9c62b6f') {
+  console.log('TEST PASSED');
+} else {
+  console.log('problem in browser for hashesjs');
+}
 
-console.log('SHA512: ', SHA512);
