162820270207cb68ea91c61d0bb9f0c71b40754c
Ali Watters <ali.watters@gmail.com>
2015-11-23 09:55:22 -0800

    illustrate class in class.

--- c++/doggy-daycare/doggies2.cpp
+++ c++/doggy-daycare/doggies2.cpp
@@ -5,14 +5,22 @@ using namespace std;
 
 const int CAPACITY = 5;
 
-struct Dog
+class Dog // this version is class within a class
 {
-	string name; // for illustration name is assumed unique
+	private: 
+		string _name; // for illustration name is assumed unique
+		int _weight;
 
-	Dog( string n = "" )
-	{
-		name = n;
-	}
+	public:
+		string name();
+		int weight();
+
+		// constructor
+		Dog( string n = "", int w = 50 )
+		{
+			_name = n;
+			_weight = w;
+		}
 
 };
 
@@ -20,10 +28,11 @@ class DayCare
 {
 	public:
 		Dog pool[CAPACITY];
+
 		int count;
 
 	public:
-		void dropOffDog(string name);	// someone drops a dog off at the daycare
+		void dropOffDog(string name, int weight );	// someone drops a dog off at the daycare
 		void pickUpDog();	// someone picks up a dog
 		
 		//constructors
@@ -45,20 +54,20 @@ int main ()
 
 	dogHappyHome.printContents(); // print the contents of our doggy care
 
-	dogHappyHome.dropOffDog("Fido");
-	dogHappyHome.dropOffDog("Fifi");
+	dogHappyHome.dropOffDog("Fido", 80);
+	dogHappyHome.dropOffDog("Fifi", 10);
 
 	dogHappyHome.printContents();
 
 
 	dogHappyHome.pickUpDog();
-	dogHappyHome.dropOffDog("Rex");
-	dogHappyHome.dropOffDog("Rover");
-	dogHappyHome.dropOffDog("Lassie");
-	dogHappyHome.dropOffDog("Dexter");
+	dogHappyHome.dropOffDog("Rex", 100);
+	dogHappyHome.dropOffDog("Rover", 150);
+	dogHappyHome.dropOffDog("Lassie", 75);
+	dogHappyHome.dropOffDog("Dexter", 40);
 
 	
-	dogHappyHome.dropOffDog("PhoPho");
+	dogHappyHome.dropOffDog("PhoPho", 5);
 
 	dogHappyHome.pickUpDog();
 	// here - do a while - get a dropOff pickUp of quit from stdin.
@@ -68,15 +77,15 @@ int main ()
 	return 0;
 }
 
-void DayCare::dropOffDog(string n) 
+void DayCare::dropOffDog(string n, int w) 
 {
 	cout << count << "\tof\t" << CAPACITY << "\n";
 	if (count >= CAPACITY) {
 		return;
 	}
-	cout << "Drop Off\t" << count << " " << n << "\n";
+	cout << "Drop Off\t" << count << " " << n << w << "\n";
 
-	pool[count] = Dog(n); // add dog to the pool in position count.
+	pool[count] = Dog(n, w); // add dog to the pool in position count.
 	count++;
 	
 	return;
@@ -89,7 +98,7 @@ void DayCare::pickUpDog()
 		return;
 	}
 	count--;
-	cout << "Pick Up\t" << pool[count].name << "\n";
+	cout << "Pick Up\t" << pool[count].name() << "\n";
 	
 	pool[count] = Dog(""); // add an empty dog
 
@@ -100,9 +109,17 @@ void DayCare::printContents()
 {
 	cout << setw(15) << left << "\nDoggy Day Care\n==============\n";
 	for( int i = 0; i < CAPACITY; i++ ) {
-		cout << setw(15) << left << i << "\t" << pool[i].name << "\n";
+		cout << setw(15) << left << i << "\t" << pool[i].name() << "\t" << pool[i].weight() << "lbs" << "\n";
 	}
 	cout << "\n";
 	return;
 }
 	
+
+int Dog::weight() {
+	return _weight;
+}
+
+string Dog::name() {
+	return _name;
+}
