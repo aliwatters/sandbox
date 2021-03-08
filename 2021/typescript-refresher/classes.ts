223ca52817fd8556a55c855accef71507f0a0efe
Ali Watters <ali.watters@gmail.com>
2021-03-08 08:19:31 -0800

    completed lesson 49

--- 2021/typescript-refresher/classes.ts
+++ 2021/typescript-refresher/classes.ts
@@ -1,4 +1,8 @@
 class Vehicle {
+  constructor(public color: string = 'red') {
+    this.color = color;
+  }
+
   public drive(): void {
     console.log('chugga chugga');
   }
@@ -7,12 +11,20 @@ class Vehicle {
     console.log('beep beep');
   }
 
-  public summary(): void {
-    console.log('I ');
+  public summary(): string {
+    return `I am a vehicle and I am ${this.color}`;
+  }
+
+  public toString(): string {
+    return this.summary();
   }
 }
 
 class TheCar extends Vehicle {
+  constructor(public wheels: number, public color: string) {
+    super(color);
+  }
+
   // project has Car already.
   public drive(): void {
     console.log('vrm vrroom');
@@ -29,11 +41,14 @@ class TheCar extends Vehicle {
   }
 }
 
-const myCar3 = new Vehicle();
+const myCar3 = new Vehicle('yellow');
 
 myCar3.drive();
 // myCar3.honk(); // protected
 
-const myCar4 = new TheCar();
+const myCar4 = new TheCar(4, 'blue');
 
 myCar4.startDriving();
+
+console.log(myCar3, `Summary: ${myCar3}`, 'from toString()');
+console.log(myCar4, `Summary: ${myCar4}`, 'from toString()');
